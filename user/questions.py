08a548a9bde2ebef19857073d7d700567062950e
timnirmal <timnirmal@gmail.com>
2024-09-20 22:04:19 +0530

    new questions

--- user/questions.py
+++ user/questions.py
@@ -2,6 +2,32 @@ from datetime import datetime
 
 import streamlit as st
 
+# Nationalities list
+nationalities = [
+    "Afghan", "Albanian", "Algerian", "American", "Andorran", "Angolan", "Antiguan", "Argentine", "Armenian",
+    "Australian", "Austrian", "Azerbaijani", "Bahamian", "Bahraini", "Bangladeshi", "Barbadian", "Belarusian",
+    "Belgian", "Belizean", "Beninese", "Bhutanese", "Bolivian", "Bosnian", "Botswanan", "Brazilian", "British",
+    "Bruneian", "Bulgarian", "Burkinabe", "Burmese", "Burundian", "Cambodian", "Cameroonian", "Canadian",
+    "Cape Verdean", "Central African", "Chadian", "Chilean", "Chinese", "Colombian", "Comoran", "Congolese",
+    "Costa Rican", "Croatian", "Cuban", "Cypriot", "Czech", "Danish", "Djiboutian", "Dominican", "Dutch",
+    "East Timorese", "Ecuadorean", "Egyptian", "Emirati", "Equatorial Guinean", "Eritrean", "Estonian", "Ethiopian",
+    "Fijian", "Filipino", "Finnish", "French", "Gabonese", "Gambian", "Georgian", "German", "Ghanaian", "Greek",
+    "Grenadian", "Guatemalan", "Guinean", "Guinea-Bissauan", "Guyanese", "Haitian", "Herzegovinian", "Honduran",
+    "Hungarian", "Icelander", "Indian", "Indonesian", "Iranian", "Iraqi", "Irish", "Israeli", "Italian", "Ivorian",
+    "Jamaican", "Japanese", "Jordanian", "Kazakhstani", "Kenyan", "Kiribati", "Korean", "Kosovar", "Kuwaiti",
+    "Kyrgyz", "Lao", "Latvian", "Lebanese", "Liberian", "Libyan", "Liechtensteiner", "Lithuanian", "Luxembourger",
+    "Macedonian", "Malagasy", "Malawian", "Malaysian", "Maldivian", "Malian", "Maltese", "Marshallese",
+    "Mauritanian", "Mauritian", "Mexican", "Micronesian", "Moldovan", "Monacan", "Mongolian", "Moroccan",
+    "Mozambican", "Namibian", "Nauruan", "Nepalese", "New Zealander", "Nicaraguan", "Nigerien", "Nigerian",
+    "Norwegian", "Omani", "Pakistani", "Palauan", "Panamanian", "Papua New Guinean", "Paraguayan", "Peruvian",
+    "Polish", "Portuguese", "Qatari", "Romanian", "Russian", "Rwandan", "Saint Lucian", "Salvadoran", "Samoan",
+    "San Marinese", "Sao Tomean", "Saudi", "Senegalese", "Serbian", "Seychellois", "Sierra Leonean", "Singaporean",
+    "Slovakian", "Slovenian", "Solomon Islander", "Somali", "South African", "Spanish", "Sri Lankan", "Sudanese",
+    "Surinamer", "Swazi", "Swedish", "Swiss", "Syrian", "Taiwanese", "Tajik", "Tanzanian", "Thai", "Togolese",
+    "Tongan", "Trinidadian", "Tunisian", "Turkish", "Tuvaluan", "Ugandan", "Ukrainian", "Uruguayan", "Uzbek",
+    "Vanuatuan", "Venezuelan", "Vietnamese", "Yemeni", "Zambian", "Zimbabwean"
+]
+
 
 def update_profile(user, users_collection, db):
     st.subheader("Update Profile")
@@ -32,31 +58,51 @@ def update_profile(user, users_collection, db):
     # Current Location
     new_location = st.text_input("Current Location", profile.get('location', ''))
 
-    # Nationalities list
-    nationalities = [
-        "Afghan", "Albanian", "Algerian", "American", "Andorran", "Angolan", "Antiguan", "Argentine", "Armenian",
-        "Australian", "Austrian", "Azerbaijani", "Bahamian", "Bahraini", "Bangladeshi", "Barbadian", "Belarusian",
-        "Belgian", "Belizean", "Beninese", "Bhutanese", "Bolivian", "Bosnian", "Botswanan", "Brazilian", "British",
-        "Bruneian", "Bulgarian", "Burkinabe", "Burmese", "Burundian", "Cambodian", "Cameroonian", "Canadian",
-        "Cape Verdean", "Central African", "Chadian", "Chilean", "Chinese", "Colombian", "Comoran", "Congolese",
-        "Costa Rican", "Croatian", "Cuban", "Cypriot", "Czech", "Danish", "Djiboutian", "Dominican", "Dutch",
-        "East Timorese", "Ecuadorean", "Egyptian", "Emirati", "Equatorial Guinean", "Eritrean", "Estonian", "Ethiopian",
-        "Fijian", "Filipino", "Finnish", "French", "Gabonese", "Gambian", "Georgian", "German", "Ghanaian", "Greek",
-        "Grenadian", "Guatemalan", "Guinean", "Guinea-Bissauan", "Guyanese", "Haitian", "Herzegovinian", "Honduran",
-        "Hungarian", "Icelander", "Indian", "Indonesian", "Iranian", "Iraqi", "Irish", "Israeli", "Italian", "Ivorian",
-        "Jamaican", "Japanese", "Jordanian", "Kazakhstani", "Kenyan", "Kiribati", "Korean", "Kosovar", "Kuwaiti",
-        "Kyrgyz", "Lao", "Latvian", "Lebanese", "Liberian", "Libyan", "Liechtensteiner", "Lithuanian", "Luxembourger",
-        "Macedonian", "Malagasy", "Malawian", "Malaysian", "Maldivian", "Malian", "Maltese", "Marshallese",
-        "Mauritanian", "Mauritian", "Mexican", "Micronesian", "Moldovan", "Monacan", "Mongolian", "Moroccan",
-        "Mozambican", "Namibian", "Nauruan", "Nepalese", "New Zealander", "Nicaraguan", "Nigerien", "Nigerian",
-        "Norwegian", "Omani", "Pakistani", "Palauan", "Panamanian", "Papua New Guinean", "Paraguayan", "Peruvian",
-        "Polish", "Portuguese", "Qatari", "Romanian", "Russian", "Rwandan", "Saint Lucian", "Salvadoran", "Samoan",
-        "San Marinese", "Sao Tomean", "Saudi", "Senegalese", "Serbian", "Seychellois", "Sierra Leonean", "Singaporean",
-        "Slovakian", "Slovenian", "Solomon Islander", "Somali", "South African", "Spanish", "Sri Lankan", "Sudanese",
-        "Surinamer", "Swazi", "Swedish", "Swiss", "Syrian", "Taiwanese", "Tajik", "Tanzanian", "Thai", "Togolese",
-        "Tongan", "Trinidadian", "Tunisian", "Turkish", "Tuvaluan", "Ugandan", "Ukrainian", "Uruguayan", "Uzbek",
-        "Vanuatuan", "Venezuelan", "Vietnamese", "Yemeni", "Zambian", "Zimbabwean"
-    ]
+    # Passport Details
+    st.subheader("Passport Details")
+    new_family_name = st.text_input("Family Name (as in Passport)", profile.get('passport_family_name', ''))
+    new_given_name = st.text_input("Given Name (as in Passport)", profile.get('passport_given_name', ''))
+    new_sex = st.radio("Sex", gender_options, index=gender_options.index(profile.get('sex', 'Male')))
+    new_passport_country_of_birth = st.selectbox("Country of Birth", nationalities)
+    new_passport_country = st.selectbox("Country of Passport", nationalities)
+
+    # Citizenship
+    st.subheader("Citizenship")
+    is_citizen_of_passport_country = st.radio("Is the client a citizen of the country of passport?", ["Yes", "No"],
+                                              index=0 if profile.get('citizenship_passport', 'Yes') == "Yes" else 1)
+
+    # Show list of countries for citizenship if client is a citizen of other countries
+    is_citizen_of_other_country = st.radio("Is the client a citizen of any other country?", ["Yes", "No"],
+                                           index=0 if profile.get('citizenship_other', 'No') == "No" else 1)
+
+    if is_citizen_of_other_country == "Yes":
+        st.write("Add countries of citizenship:")
+        citizenship_list = profile.get('list_countries_of_citizenship', [])
+        selected_countries = st.multiselect("Select Countries", nationalities, default=citizenship_list)
+
+        # Add a button to allow users to add more countries
+        if st.button("Add another country"):
+            selected_countries.append(st.selectbox("Select Additional Country", nationalities))
+
+    # Other Passports
+    st.subheader("Other Passports")
+    has_other_passports = st.radio("Does the client have other current passports?", ["Yes", "No"],
+                                   index=0 if profile.get('other_passports', 'No') == "No" else 1)
+
+    if has_other_passports == "Yes":
+        country_of_other_passport = st.selectbox("Country of other Passport", nationalities)
+
+    usual_country_of_residence = st.selectbox("Usual Country of Residence", nationalities)
+
+    # Relationship Status based on partner
+    has_partner = st.radio("Does the client have a partner?", ["Yes", "No"],
+                           index=0 if profile.get('partner', 'No') == "No" else 1)
+
+    if has_partner == "Yes":
+        relationship_status = st.selectbox("Relationship Status", ["Married", "DE FACTO"])
+    else:
+        relationship_status = st.selectbox("Relationship Status", ["Divorced", "Engaged", "Never Married",
+                                                                   "Permanently Separated", "Widowed"])
 
     # Convert to lowercase
     nationalities_lowercase = [nationality.lower() for nationality in nationalities]
